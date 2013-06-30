7cfc42d3e41a28e1a0f72eb3f00b990dd2d59cad
Eric Iacutone <eric.iacutone@gmail.com>
2013-06-30 18:27:43 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graffiti::Application.config.secret_token = '596047c2095b7321f92a98e0cd4540c922ee2d902851acff06fde3ecf3f7d504d0032517fdf2335406926ca101155c9faa7bd0503e5fc99137f264a020b08e7f'
