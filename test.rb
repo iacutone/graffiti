fcad62d029d80b42367e1492621538bb0d4abdac
Eric Iacutone <eric.iacutone@gmail.com>
2013-07-13 18:03:22 -0400

    readme updated

--- test.rb
+++ test.rb
@@ -1,20 +1,20 @@
-# require 'httparty'
-# require 'json'
-# require 'pry'
-
+require 'httparty'
+require 'json'
+require 'pry'
 
 response = HTTParty.get('http://data.cityofnewyork.us/resource/2j99-6h29.json')
+  	content = response.body
+  	json_content = JSON.parse(content)
+  	json_content.each do |item|
+  		 # created_date = item["created_date"]
+  		 # closed_date = item["closed_date"]
+  		 # resolution_action = item["resolution_action"]
+  		 # status = item["status"]
+  		 # incident_address_display = item["incident_address_display"]
+  		 borough = item["borough"]
+  		 # x_coordinate = item["x_coordinate"]
+  		 # y_coordinate = item[":y_coordinate"]
 
-# hash = {}
-# array = []
-# response.each do |item|
-# 	array << item['created_date']
-# 	array << item['closed_date']
-# 	array << item['resolution_action']
-# 	array << item['status']
-# 	array << item['incident_address_display']
-# 	array << item['borough']
-# 	array << item['y_coordinate']
-# 	array << item['x_coordinate']
-# 	binding.pry
-# end
+  		 s = Location.new(borough: borough)
+  		 s.save
+  		end
